5b418e2bb812200951452adfd9c3e9273efaa556
piyushdevcode <17671660+piyushdevcode@users.noreply.github.com>
2021-11-25 13:59:36 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ export default function App() {
   return (
     <div className="App">
       <div className="pageBox">
+        <header className="navbar">
+          <div className="navbar_title navbar_item">LOGO</div>
+          <div className="navbar_item">Home</div>
+          <div className="navbar_item">About</div>
+          <div className="navbar_item">Services</div>
+          <div className="navbar_item">Blog</div>
+          <div className="navbar_item">Contact</div>
+          <div className="navbar_item">Follow Us</div>
+        </header>
         <div className="triangleLeft"></div>
         {/* <BgTriangle /> */}
-        <img src="./landing.png" alt="ola" />
+        <img src="/src/img/landing.png" alt="ola" />
       </div>
 
-      <h1 className="cname">Hey from</h1>
+      <h1 className="cname">Yash Consultants</h1>
+      <h2 className="cname tline">Yash Consultants</h2>
     </div>
   );
 }
