fa1beac45926d4d90f7c4658392619a434a69552
piyushdevcode <17671660+piyushdevcode@users.noreply.github.com>
2021-11-26 14:23:37 +0530

    major changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { ReactComponent as BgTriangle } from "./img/bgtr.svg";
 import landing from "./img/landing.png";
+
 export default function App() {
   return (
     <div className="App">
@@ -15,13 +16,28 @@ export default function App() {
           <div className="navbar_item">Follow Us</div>
         </header>
         {/* <BgTriangle style={{width:"500vw",height:"500vh",transform:"translate(-2600px,-250px)",overflow:"hidden"}}/> */}
+        {/* <BgTriangle/> */}
         {/* <img src={landing} alt="ola" /> */}
-        <h1 className="cname">Yash Consultants</h1>
-        <h2 className="cname tline">Yash Consultants</h2>
+        <div className="cname">Yash Consultants</div>
+        <div className="cname tline">Yash Consultants</div>
         <div className="btn-grp">
           <button class="btn">Employee</button>
+          <button class="btn">Employer</button>
         </div>
       </div>
     </div>
   );
+}
+
+{
+  /* aa
+  > Job Search 
+  > Feedback
+  > Newsletter
+  > Our recruiters
+  > Info Component(about)
+  > Our Services (component)
+  > Login
+  > 404 error 
+*/
 }
